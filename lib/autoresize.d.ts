d5bd21e80e58fa3ea4e534e110121208c7480715
nim579 <nim579@users.noreply.github.com>
2023-07-10 12:13:27 +0000

    deploy: 058513a50b10009454a83bc1b2c293754d11bec3

---
+++ lib/autoresize.d.ts
@@ -0,0 +1,6 @@
+declare global {
+    interface Window {
+        __embedsworks?: () => void;
+    }
+}
+export {};
---
+++ lib/utils.d.ts
@@ -0,0 +1,10 @@
+export declare const stringToBuffer: (str: string) => Uint8Array;
+export declare const bufferToString: (buf: Uint8Array) => string;
+export declare const stringToBlob: (string: string) => Promise<Blob>;
+export declare const bufferToBase64: (arraybuffer: ArrayBuffer) => string;
+export declare const base64ToBuffer: (base64string: string) => Uint8Array;
+export declare const compress: (string: string) => Promise<string>;
+export declare const decompress: (base64string: string) => Promise<string>;
+export declare const insertHtml: (el: HTMLElement, html: string) => Promise<void>;
+export declare const copyText: (data: string | Promise<string>) => Promise<void>;
+export declare const onAutoresize: (event: MessageEvent<any>) => void;
