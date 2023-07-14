f606eb7274e31d199aa7db093a2f745948a9f3a9
Nikolay Ivanushkin <nick-iv@mustapp.com>
2023-07-14 15:54:10 +0300

    Guard empty embed page

--- src/Embed.vue
+++ src/Embed.vue
@@ -1,14 +1,20 @@
 <script setup lang="ts">
-import { computed } from 'vue';
-import { useRoute } from 'vue-router';
+import { computed, watchEffect } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
 import Frame from './Frame.vue';
 
+const router = useRouter();
 const route = useRoute();
 
 const code = computed(() => {
   return route.query.code
     ? String(route.query.code)
     : null;
+});
+
+watchEffect(() => {
+  if (!route.query.code)
+    router.replace({ name: 'main' });
 });
 </script>
 
